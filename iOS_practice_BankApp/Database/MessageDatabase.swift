4b1a86fab547ac3733a94d7ea4b91e1cadf8bc8b
jerry960331 <jerry960331@gmail.com>
2020-09-08 21:56:50 +0800

    通知介面、刪除通知，準備做已讀通知

--- iOS_practice_BankApp/Database/MessageDatabase.swift
+++ iOS_practice_BankApp/Database/MessageDatabase.swift
@@ -14,16 +14,16 @@ struct MessageDatabase {
     
     private var messages: [Message]? = nil
     
-    mutating func loadData(userIndexRowInTableView index: Int) -> Message {
+    mutating func loadData(messageType:String, userIndexRowInTableView index: Int) -> Message {
         if messages == nil {
-            loadDataFromDatabase()
-            return loadData(userIndexRowInTableView: index)
+            loadDataFromDatabase(messageType: messageType)
+            return loadData(messageType: messageType, userIndexRowInTableView: index)
         } else {
             if let messages = messages {
-                return Message(UUID: messages[index].UUID, messageType: messages[index].messageType, time: messages[index].time, title: messages[index].title, content: messages[index].content)
+                return Message(UUID: messages[index].UUID, messageType: messages[index].messageType, time: messages[index].time, title: messages[index].title, content: messages[index].content, isRead: messages[index].isRead)
             } else {
                 // 應該不會發生
-                return Message(UUID: "error", messageType: "error", time: Date(), title: "title", content: "content")
+                return Message(UUID: "error", messageType: "error", time: Date(), title: "title", content: "content", isRead: false)
             }
         }
     }
@@ -32,15 +32,18 @@ struct MessageDatabase {
         messages = nil
     }
     
-    private mutating func loadDataFromDatabase() {
+    private mutating func loadDataFromDatabase(messageType: String) {
         messages = []
-        let messagesFromDB = realm.objects(RLM_Message.self)
+        let messagesFromDB = realm.objects(RLM_Message.self).sorted(byKeyPath: "time", ascending: false)
         for messageFromDB in messagesFromDB {
-            messages?.append(Message(UUID: messageFromDB.uuid,
+            if messageFromDB.messageType == messageType {
+                messages?.append(Message(UUID: messageFromDB.uuid,
                                      messageType: messageFromDB.messageType,
                                      time: messageFromDB.time,
                                      title: messageFromDB.title,
-                                     content: messageFromDB.content))
+                                     content: messageFromDB.content,
+                                     isRead: messageFromDB.isRead))
+            }
         }
     }
     
@@ -58,13 +61,59 @@ struct MessageDatabase {
         message.time = Date.init()
         message.title = "測試訊息"
         message.content = "此訊息為測試訊息，標題為測試訊息。補字補字補字補字補字補字補字補字補字"
-        
+        print(message.messageType + "訊息已創建")
         
         try! realm.write {
             realm.add(message)
         }
     }
-    // todo getDataCount
+    
+    mutating func getMessageCount(messageType: String) -> Int {
+        
+        if messages == nil {
+            loadDataFromDatabase(messageType: messageType)
+            return getMessageCount(messageType: messageType)
+        } else {
+            var count = 0
+            if let messages = messages {
+                for message in messages {
+                    if message.messageType == messageType {
+                        count += 1
+                    }
+                }
+                return count
+            } else {
+                // 如果 message 沒有被初始化
+                print("message 沒有被初始化")
+                return 0
+            }
+        }
+    }
+    
+    mutating func readMessage(messageType:String, userIndexRowInTableView index: Int) {
+        //todo 已讀訊息
+    }
+    
+    mutating func deleteMessage(messageType:String, userIndexRowInTableView index: Int) {
+        
+        //todo index to uuid
+        
+        
+        // 確保 messages 存在
+        if let messages = messages {
+            print(messages[index])
+            
+            let message = realm.objects(RLM_Message.self).filter("uuid = %@", messages[index].UUID)
+            
+            try! realm.write {
+                realm.delete(message)
+            }
+        }
+        
+
+    }
+    
+    
 }
 
 public struct Message {
@@ -73,6 +122,7 @@ public struct Message {
     var time: Date = Date()
     var title: String = ""
     var content: String = ""
+    var isRead: Bool
 }
 
 class RLM_Message : Object {
@@ -84,6 +134,7 @@ class RLM_Message : Object {
     @objc dynamic var time: Date = Date()
     @objc dynamic var title: String = ""
     @objc dynamic var content: String = ""
+    @objc dynamic var isRead: Bool = false
     
     //設置索引主鍵
     override static func primaryKey() -> String? {
--- iOS_practice_BankApp/ViewController/Navigationbar/NotificationViewController.swift
+++ iOS_practice_BankApp/ViewController/Navigationbar/NotificationViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class NotificationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var notificationSettingButton: UIButton!
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var messageTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
+    
+    var messageDatabase = MessageDatabase()
+    
+    var messageType = "個人訊息"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,14 +26,61 @@ class NotificationViewController: UIViewController, UITableViewDelegate, UITable
     }
     
     
+    @IBAction func notificationSettingButtonClicked(_ sender: Any) {
+        messageDatabase.createMessage()
+        tableViewReloadDataAndClearLocalMessages()
+    }
+    
+    @IBAction func editButtonClicked(_ sender: Any) {
+        tableView.setEditing(!tableView.isEditing, animated: true)
+        editButton.setTitle((self.tableView.isEditing) ? "完成編輯" : "編輯訊息", for: .normal)
+        tableView.reloadData()
+    }
+    
+
+    @IBAction func messageTypeSegmentedControlValueChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            messageType = "個人訊息"
+        } else {
+            messageType = "好康優惠"
+        }
+        tableViewReloadDataAndClearLocalMessages()
+    }
+    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return messageDatabase.getMessageCount(messageType: messageType)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NotificationTableViewCell
+        let message = messageDatabase.loadData(messageType: messageType, userIndexRowInTableView: indexPath.row)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        cell.timeLabel.text = dateFormatter.string(from: message.time)
+        cell.titleLabel.text = message.title
+        cell.contentLabel.text = message.content
         
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // todo 儲存已讀
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            print("deleted")
+            
+            messageDatabase.deleteMessage(messageType: messageType, userIndexRowInTableView: indexPath.row)
+            tableViewReloadDataAndClearLocalMessages()
+            
+        }
+    }
+    
+    func tableViewReloadDataAndClearLocalMessages(){
+        messageDatabase.clearLocalMessages()
+        tableView.reloadData()
+    }
 }
